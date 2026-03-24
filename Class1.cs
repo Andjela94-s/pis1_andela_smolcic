834453106642a5d1ddc33e5603d9a3be0f5a2f00
condaguru <jakov.markic358@gmail.com>
2026-03-24 18:01:25 +0100

    tght

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pis1_andela_smolcic
+{
+    internal class Class1
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,3 +5,4 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Hello");
 Console.WriteLine("Novo");
+//komentar
